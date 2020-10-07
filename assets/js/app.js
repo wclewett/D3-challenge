2f60097ca40c08b29beb9a1a6e920f8fa664c185
wclewett <wclewett@gmail.com>
2020-10-07 19:37:06 -0400

    Styling and Static Plot

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,17 +20,128 @@ var chartHeight = svgHeight - margin.top - margin.bottom;
 var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
-  .attr("height", svgHeight);
+  .attr("height", svgHeight)
+  .attr("fill", "white");
 
 // Append a group area, then set its margins
 var chartGroup = svg.append("g")
-.attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+// Set initial axis names
+var xLabel = "poverty"
+var yLabel = "healthcare"
 
 // Load data from .csv file
 d3.csv("assets/data/data.csv").then(function(data) {
 
+    var statesData = data;
     // Print the data
-    console.log(data);
+    console.log(statesData);
+
+    // Parse data and cast to numeric
+    statesData.forEach(function(data) {
+        data.poverty    = +data.poverty;
+        data.healthcare = +data.healthcare;
+        data.age        = +data.age;
+        data.smokes     = +data.smokes;
+        data.obesity    = +data.obesity;
+        data.income     = +data.income;
+      });
+    
+    // Configure a linear scale with a range between the chartHeight and 0
+    // Set the domain for the xLinearScale function
+    var xLinearScale = d3.scaleLinear()
+        .range([0, chartWidth])
+        .domain([d3.min(statesData, data => data[xLabel]) - 2, d3.max(statesData, data => data[xLabel]) + 2]);
+
+    // Configure a linear scale with a range between the chartHeight and 0
+    // Set the domain for the yLinearScale function
+    var yLinearScale = d3.scaleLinear()
+        .range([chartHeight, 0])
+        .domain([d3.min(statesData, data => data[yLabel]) - 2, d3.max(statesData, data => data[yLabel]) + 2]);
+
+    // Initialize axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+      
+    // Append x and y axes to the chart
+    var xAxis = chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
+
+    var yAxis = chartGroup.append("g")
+        .call(leftAxis);
+
+    // Create scatterplot and append initial circles
+    var circlesGroup = chartGroup.selectAll("g circle")
+        .data(statesData)
+        .enter()
+        .append("g")
+
+    // Append (x, y) locations for circles
+    var circlesLoc = circlesGroup.append("circle")
+        .attr("cx", d => xLinearScale(d[xLabel]))
+        .attr("cy", d => yLinearScale(d[yLabel]))
+        .attr("r", 17)
+        .classed("stateCircle", true);   
+
+    // Add labels for circles
+    var circlesLabel = circlesGroup.append("text")
+        .text(d => d.abbr)
+        .attr("dx", d => xLinearScale(d[xLabel]))
+        .attr("dy", d => yLinearScale(d[yLabel]) + 5)
+        .classed("stateText", true);
+
+    // Create group for xAxis labels
+    var xlabelsGroup = chartGroup.append("g")
+        .attr("transform", `translate(${width / 2}, ${height})`);
+
+    var povertyLabel = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 40)
+        .attr("value", "poverty") // value to grab for event listener
+        .text("In Poverty (%)")
+        .classed("active", true);
+
+    var ageLabel = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 60)
+        .attr("value", "age") // value to grab for event listener
+        .text("Age (Median)")
+        .classed("inactive", true);
+
+    var incomeLabel = xlabelsGroup.append("text")
+        .attr("x", 0)
+        .attr("y", 80)
+        .attr("value", "income") // value to grab for event listener
+        .text("Household Income (Median)")
+        .classed("inactive", true);
+
+    // Create group for yAxis labels
+    var ylabelsGroup = chartGroup.append("g");
+
+    var healthcareLabel = ylabelsGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", -40)
+        .attr("value", "healthcare") // value to grab for event listener
+        .text("Lacks Healthcare (%)")
+        .classed("active", true);
+
+    var smokesLabel = ylabelsGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", -60)
+        .attr("value", "smokes") // value to grab for event listener
+        .text("Smokes (%)")
+        .classed("inactive", true);
+
+    var obeseLabel = ylabelsGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -(height / 2))
+        .attr("y", -80)
+        .attr("value", "obesity") // value to grab for event listener
+        .text("Obese (%)")
+        .classed("inactive", true);
+
     });
-  
---
+++ assets/js/functions.js
@@ -0,0 +1,23 @@
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+  
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+};

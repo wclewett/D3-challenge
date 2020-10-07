549a5fdc7e372e1735d50c7179c739c76094de70
wclewett <wclewett@gmail.com>
2020-10-07 17:23:58 -0400

    Styling and Functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 // @TODO: YOUR CODE HERE!
+
+// Define SVG area dimensions
 var svgWidth = 960;
 var svgHeight = 500;
 
+// Define the chart's margins as an object
 var margin = {
     top: 20,
     right: 40,
@@ -9,5 +12,25 @@ var margin = {
     left: 100
   };
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Select body, append SVG area to it, and set its dimensions
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append a group area, then set its margins
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+// Load data from .csv file
+d3.csv("assets/data/data.csv").then(function(data) {
+
+    // Print the data
+    console.log(data);
+    });
+  
